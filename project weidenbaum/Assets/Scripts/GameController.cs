543e557226b796797fa66cac3c5086de331f6ecd
JakubMahdal <63870600+JakubMahdal@users.noreply.github.com>
2020-04-17 21:42:22 +0200

    Update 1.1

--- project weidenbaum/Assets/Scripts/GameController.cs
+++ project weidenbaum/Assets/Scripts/GameController.cs
@@ -1,10 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     public Question[] questions;
     public int zivoty = 3;
+    public TextMeshProUGUI otazka;
+    public TextMeshProUGUI[] odpovedi;
+    private void Start()
+    {
+        Otazky();
+    }
+    public void Otazky()
+    {
+        List<int> indexy = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            indexy.Add(i);
+        }
+        int x = Random.Range(0,questions.Length);
+        string zvolenaotazka = questions[x].question;
+        string sodpvoved = questions[x].answer;
+        otazka.text = zvolenaotazka;
+        int random = Random.Range(0, 3);
+        indexy.RemoveAt(random);
+        odpovedi[random].text = sodpvoved;
+        for (int i = 0; i < 3; i++)
+        {
+                odpovedi[indexy[i]].text = questions[x].wrongAnswers[i];
+        }
+        
+    }
 }
 
--- project weidenbaum/Assets/Scripts/Question.cs
+++ project weidenbaum/Assets/Scripts/Question.cs
@@ -5,3 +5,4 @@ public class Question
     public string answer;
     public string[] wrongAnswers = new string[3];
 }
+
