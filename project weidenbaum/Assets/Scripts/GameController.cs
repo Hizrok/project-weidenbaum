2793b38521bc5ebe9c254936f8713568a5b54193
JakubMahdal <63870600+JakubMahdal@users.noreply.github.com>
2020-04-18 19:55:23 +0200

    1.2

--- project weidenbaum/Assets/Scripts/GameController.cs
+++ project weidenbaum/Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ public class GameController : MonoBehaviour
 {
     public Question[] questions;
     public int zivoty = 3;
-
+    public List<int> indexyotazek = new List<int>();
     public TextMeshProUGUI otazka;
     public TextMeshProUGUI[] odpovedi;
 
@@ -17,39 +17,53 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         Otazky();
+        GetNextQuestion();
     }
 
     private void Update()
     {
-        if (timeStart >= 0)
+        if (indexyotazek.Count != 0)
         {
-            timeStart -= Time.deltaTime;
-            timer.text = Mathf.Round(timeStart).ToString();
-        } else
-        {
-            Otazky();
-            timeStart = 10f;
+
+            if (timeStart >= 0)
+            {
+                timeStart -= Time.deltaTime;
+                timer.text = Mathf.Round(timeStart).ToString();
+            }
+            else
+            {
+                GetNextQuestion();
+                timeStart = 10f;
+            }
         }
     }
-
-    public void Otazky()
+    public void GetNextQuestion()
     {
         List<int> indexy = new List<int>();
         for (int i = 0; i < 4; i++)
         {
             indexy.Add(i);
         }
-        int x = Random.Range(0,questions.Length);
-        string zvolenaotazka = questions[x].question;
-        string sodpvoved = questions[x].answer;
+        int x = Random.Range(0, indexyotazek.Count);
+        string zvolenaotazka = questions[indexyotazek[x]].question;
+        string sodpvoved = questions[indexyotazek[x]].answer;
         otazka.text = zvolenaotazka;
         int random = Random.Range(0, 3);
         indexy.RemoveAt(random);
         odpovedi[random].text = sodpvoved;
-        
         for (int i = 0; i < 3; i++)
         {
-                odpovedi[indexy[i]].text = questions[x].wrongAnswers[i];
+            odpovedi[indexy[i]].text = questions[indexyotazek[x]].wrongAnswers[i];
+        }
+        indexyotazek.RemoveAt(x);
+    }
+
+    public void Otazky()
+    {
+        
+        for (int i = 0; i < questions.Length; i++)
+        {
+            indexyotazek.Add(i);
         }
         
     }
