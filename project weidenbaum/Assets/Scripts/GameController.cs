35f7f9d19ad0c410589a4d70e6500547015697ec
Hizrok <hizrok@seznam.cz>
2020-04-18 19:24:39 +0200

    Update 1.3

--- project weidenbaum/Assets/Scripts/GameController.cs
+++ project weidenbaum/Assets/Scripts/GameController.cs
@@ -7,12 +7,31 @@ public class GameController : MonoBehaviour
 {
     public Question[] questions;
     public int zivoty = 3;
+
     public TextMeshProUGUI otazka;
     public TextMeshProUGUI[] odpovedi;
+
+    public TextMeshProUGUI timer;
+    public float timeStart = 10f; 
+    
     private void Start()
     {
         Otazky();
     }
+
+    private void Update()
+    {
+        if (timeStart >= 0)
+        {
+            timeStart -= Time.deltaTime;
+            timer.text = Mathf.Round(timeStart).ToString();
+        } else
+        {
+            Otazky();
+            timeStart = 10f;
+        }
+    }
+
     public void Otazky()
     {
         List<int> indexy = new List<int>();
@@ -27,6 +46,7 @@ public class GameController : MonoBehaviour
         int random = Random.Range(0, 3);
         indexy.RemoveAt(random);
         odpovedi[random].text = sodpvoved;
+        
         for (int i = 0; i < 3; i++)
         {
                 odpovedi[indexy[i]].text = questions[x].wrongAnswers[i];
