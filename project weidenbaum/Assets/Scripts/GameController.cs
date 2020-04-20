49415b4eb36eb6c502204e3f667e50a34b5b23f3
Hizrok <hizrok@seznam.cz>
2020-04-20 19:27:48 +0200

    Update 1.5
    - random questions and answers

--- project weidenbaum/Assets/Scripts/GameController.cs
+++ project weidenbaum/Assets/Scripts/GameController.cs
@@ -6,23 +6,29 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public Question[] questions;
-    public int zivoty = 3;
-    public List<int> indexyotazek = new List<int>();
-    public TextMeshProUGUI otazka;
-    public TextMeshProUGUI[] odpovedi;
+    public int lives = 3;
+    public List<int> questionIndex = new List<int>();
+    public TextMeshProUGUI questionTextBox;
+    public TextMeshProUGUI[] answerTextBoxes;
 
     public TextMeshProUGUI timer;
-    public float timeStart = 10f; 
+    public float start = 10f;
+    private float timeStart; 
     
     private void Start()
     {
-        Otazky();
+        timeStart = start;
+        for (int i = 0; i < questions.Length; i++)
+        {
+            questionIndex.Add(i);
+        }
+
         GetNextQuestion();
     }
 
     private void Update()
     {
-        if (indexyotazek.Count != 0)
+        if (questionIndex.Count != 0)
         {
 
             if (timeStart >= 0)
@@ -33,39 +39,32 @@ public class GameController : MonoBehaviour
             else
             {
                 GetNextQuestion();
-                timeStart = 10f;
+                timeStart = start;
             }
         }
     }
     public void GetNextQuestion()
     {
-        List<int> indexy = new List<int>();
+        int x = Random.Range(0, questionIndex.Count);
+
+        string chosenQuestion = questions[questionIndex[x]].question;
+        string chosenAnswer = questions[questionIndex[x]].answer;
+        questionTextBox.text = chosenQuestion;
+        
+        int random = Random.Range(0, 3);
+        answerTextBoxes[random].text = chosenAnswer;
+
         for (int i = 0; i < 4; i++)
         {
-            indexy.Add(i);
+            if (i > random)
+            {
+                answerTextBoxes[i].text = questions[questionIndex[x]].wrongAnswers[i-1];
+            } else if (i < random)
+            {
+                answerTextBoxes[i].text = questions[questionIndex[x]].wrongAnswers[i];
+            }
         }
-        int x = Random.Range(0, indexyotazek.Count);
-        string zvolenaotazka = questions[indexyotazek[x]].question;
-        string sodpvoved = questions[indexyotazek[x]].answer;
-        otazka.text = zvolenaotazka;
-        int random = Random.Range(0, 3);
-        indexy.RemoveAt(random);
-        odpovedi[random].text = sodpvoved;
-        for (int i = 0; i < 3; i++)
-        {
-            odpovedi[indexy[i]].text = questions[indexyotazek[x]].wrongAnswers[i];
-        }
-        indexyotazek.RemoveAt(x);
-    }
-
-    public void Otazky()
-    {
-        
-        for (int i = 0; i < questions.Length; i++)
-        {
-            indexyotazek.Add(i);
-        }
-        
+        questionIndex.RemoveAt(x);
     }
 }
 
